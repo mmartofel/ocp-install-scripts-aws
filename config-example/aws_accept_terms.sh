8adaae73147443923d524907aaee018c5b887779
mmartofel <mmartofe@redhat.com>
2025-01-27 15:57:54 +0100

    accept terms from aws cli, perhaps not possible

---
+++ config-example/aws_accept_terms.sh
@@ -0,0 +1,8 @@
+echo Before using Red Hat OpenShift marketplace offering you need to 'Accept Terms'
+echo You can do it via web interface, for example tor Red Hat OpenShift Kubernetes Engine at:
+echo
+echo https://aws.amazon.com/marketplace/server/procurement?productId=963b36c3-de6f-48ed-b802-2b38b2a2cdeb
+echo
+echo myself can't find the way to accept terms from command line or API, I'm not skilled enough with AWS
+echo possibly terraform may be of help here
+
