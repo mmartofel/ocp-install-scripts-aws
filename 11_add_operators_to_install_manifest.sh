20888faeaf83b912bcc7da360f51d3ba5ffd737c
mmartofel <mmartofe@redhat.com>
2025-03-15 22:49:28 +0100

    add operators and ACM setup

---
+++ 11_add_operators_to_install_manifest.sh
@@ -0,0 +1,5 @@
+# If you need to install operators to your install manifest put subscriptions yaml files into ./operators directory 
+# then copy them over to ./manifest/manifests directory
+
+cp ./operators/* ./manifest/manifests
+
